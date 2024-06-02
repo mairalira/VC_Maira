bc09e018e092b2544982409143e1270b13dc8c5c
mairalira <103345183+mairalira@users.noreply.github.com>
2024-06-02 20:59:51 -0300

    Update data.py

--- modules/data.py
+++ modules/data.py
@@ -111,7 +111,7 @@ def get_cifar10(datapath='../../data/', download=True):
                                      transform=transforms.Compose([
                                          transforms.RandomCrop(32, padding=4),
                                          transforms.RandomResizedCrop(224),
-                                         transforms.RandomHoizontalFlip(),
+                                         transforms.RandomHorizontalFlip(),
                                          transforms.ToTensor(),
                                          normalize
                                      ]),
