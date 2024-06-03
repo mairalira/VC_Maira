01ba2467fd6a5779591ab1b1e8cfbea649fd9f43
mairalira <103345183+mairalira@users.noreply.github.com>
2024-06-03 20:51:50 -0300

    Update heatmap.py

--- modules/heatmap.py
+++ modules/heatmap.py
@@ -70,18 +70,24 @@ def combine_heatmaps(heatmap_paths, save_dir, combined_heatmap_name='combined_he
     combined_heatmap_path = os.path.join(save_dir, combined_heatmap_name)
     imageio.imwrite(combined_heatmap_path, combined_heatmap)
 
-def generate_and_combine_heatmaps(model, dataloader, save_dir):
+def generate_and_combine_heatmaps(self, all_data, all_output, all_target, save_dir):
+    # Create a dataloader from the concatenated data
+    concatenated_dataset = torch.utils.data.TensorDataset(all_data, all_target)
+    concatenated_loader = torch.utils.data.DataLoader(concatenated_dataset, batch_size=1, shuffle=False)
+
     # Generate and save individual heatmaps
     heatmap_paths = []
-    for i, (images, _) in enumerate(dataloader):
+    for i, (images, targets) in enumerate(concatenated_loader):
         for j in range(images.shape[0]):
             image = images[j]
-            image_name = f'image_{i * dataloader.batch_size + j}'
-            generate_individual_heatmap(model, image, save_dir, image_name)
-            heatmap_paths.append(os.path.join(save_dir, f'{image_name}_heatmap.png'))
+            target = targets[j]
+            image_name = f'image_{i * len(images) + j}'
+            heatmap_path = self.generate_individual_heatmap(image, target, save_dir, image_name)
+            heatmap_paths.append(heatmap_path)
 
     # Combine individual heatmaps into one
-    combine_heatmaps(heatmap_paths, save_dir)
+    combined_heatmap_path = self.combine_heatmaps(heatmap_paths, save_dir)
+    return combined_heatmap_path
 
 if __name__ == '__main__':
     # Example usage
