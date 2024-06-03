75df33a092ff1c158c9f307ff1443d752d62fb1a
mairalira <103345183+mairalira@users.noreply.github.com>
2024-06-03 12:14:28 -0300

    Update main.py
    changes in default epochs, batch size, total pruning and pruning rate

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ def get_args():
 
     parser.add_argument('--arch', default='vgg16', metavar='ARCH',
                         help='model architecture: resnet18, resnet50, vgg16, alexnet')
-    parser.add_argument('--train-batch-size', type=int, default=100, metavar='N',
+    parser.add_argument('--train-batch-size', type=int, default=32, metavar='N',
                         help='input batch size for training (default: 32)')
-    parser.add_argument('--test-batch-size', type=int, default=100, metavar='N',
+    parser.add_argument('--test-batch-size', type=int, default=32, metavar='N',
                         help='input batch size for testing (default: 20)')
     parser.add_argument('--trialnum', type=int, default=1, metavar='N',
                         help='trial number (default: 1)')
-    parser.add_argument('--epochs', type=int, default=100, metavar='N',
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
                         help='learning rate (default: 0.001)')
@@ -54,10 +54,10 @@ def get_args():
     parser.add_argument('--method-type', type=str, default='lrp', metavar='N',
                         help='model architecture selection: grad/taylor/weight/lrp')
 
-    parser.add_argument('--total-pr', type=float, default=9.001 / 10.0, metavar='M',
+    parser.add_argument('--total-pr', type=float, default=0.9, metavar='M',
                         help='Total pruning rate')
-    parser.add_argument('--pr-step', type=float, default=0.05, metavar='M',
-                        help='Pruning step: 0.05 (5% for each step)')
+    parser.add_argument('--pr-step', type=float, default=0.1, metavar='M',
+                        help='Pruning step: 0.1 (10% for each step)')
 
     parser.add_argument('--data-type', type=str, default='cifar10', metavar='N',
                         help='model architecture selection: cifar10/imagenet')
