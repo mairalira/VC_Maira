97b1922124be47e50cd7a9ae6c703610b5434d6c
mairalira <103345183+mairalira@users.noreply.github.com>
2024-06-10 23:09:15 -0300

    Update prune_resnet.py

--- modules/prune_resnet.py
+++ modules/prune_resnet.py
@@ -333,14 +333,14 @@ class PruningFineTuner:
             heatmap /= torch.max(heatmap)
 
             image_array = image_tensor.permute(0, 2, 3, 1).squeeze(0).cpu().numpy()
-            print('Image array shape: {image_array.shape}')
+            print('Image array shape: '{image_array.shape})
 
             # Apply colormap
             cmap = plt.get_cmap('jet')
             heatmap_cpu = heatmap.cpu().detach().numpy()
             heatmap_colored = (cmap(heatmap_cpu)[:, :, :3] * 255).astype(np.uint8)
             heatmap_colored_resized = cv2.resize(heatmap_colored, (image_array.shape[2], image_array.shape[1]))
-            print('Heatmap resized shape: {heatmap_colored_resized.shape}')
+            print('Heatmap resized shape: '{heatmap_colored_resized.shape})
             
             # Blend the heatmap with the original image
             blended_image = cv2.addWeighted(image_array, 0.6, heatmap_colored_resized, 0.4, 0)
