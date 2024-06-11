8bb87af5d82f0483f3b0e1ce03a392fa10d18ead
mairalira <103345183+mairalira@users.noreply.github.com>
2024-06-11 09:56:37 -0300

    Update prune_resnet.py

--- modules/prune_resnet.py
+++ modules/prune_resnet.py
@@ -348,7 +348,13 @@ class PruningFineTuner:
             
             # Save the blended image
             save_path = f'gradcam_results/gradcam_{image_id}.png'
+            save_original_path = f'gradcam_results/original_{image_id}.png'
+            save_heatmap_path = f'gradcam_results/heatmap_{image_id}.png'
+            
+            cv2.imwrite(save_original_path, image_array_resized)
+            cv2.imwrite(save_heatmap_path, heatmap_colored_resized)
             cv2.imwrite(save_path, blended_image)
+            
             print(f"Overlay image saved to {save_path}")
             
             
