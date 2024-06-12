c653f980b79644c609367e0b2a052850ae3ba8a2
mairalira <103345183+mairalira@users.noreply.github.com>
2024-06-12 10:20:07 -0300

    Update prune_resnet.py

--- modules/prune_resnet.py
+++ modules/prune_resnet.py
@@ -290,7 +290,7 @@ class PruningFineTuner:
         print("Test method - Epoch:", self.current_epoch)
         
         if self.args.epochs is not None and self.current_epoch != self.args.epochs:  # Check if it's the last epoch
-            return
+            return None
 
         save_dir = 'gradcam_results'
         os.makedirs(save_dir, exist_ok=True)
